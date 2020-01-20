8a2961dc09db195842f804afa3121eb696e6945c
Roodewald <55383286+Roodewald@users.noreply.github.com>
2020-01-21 00:44:23 +0500

    with keyboard

--- processing.js
+++ processing.js
@@ -1,5 +1,5 @@
 const send = require('./send-messange')
 
-module.exports = async({user_id: userid, body: text}) => {
+module.exports = async({user_id: userid, body: text, payload}) => {
     await send(userid,text)
 };
--- send-messange.js
+++ send-messange.js
@@ -1,13 +1,65 @@
 const {TOKEN} = require('./config')
 const api     = require('vk-easy')
-const keyboard = JSON.stringify({command:"start"})
+const keyboard = JSON.stringify({
+    one_time: false,
+    buttons: [
+        [{
+            action: {
+                type: "location",
+                payload: "{\"button\": \"1\"}"
+            }
+        }],
+        [{
+            action: {
+                type: "open_app",
+                app_id: 6979558,
+                owner_id: -181108510,
+                hash: "sendKeyboard",
+                label: "Отправить клавиатуру"
+            }
+        }],
+        [{
+                action: {
+                    type: "text",
+                    payload: "{\"button\": \"1\"}",
+                    label: "Negative"
+                },
+                color: "negative"
+            },
+            {
+                action: {
+                    type: "text",
+                    payload: "{\"button\": \"2\"}",
+                    label: "Positive"
+                },
+                color: "positive"
+            },
+            {
+                action: {
+                    type: "text",
+                    payload: "{\"button\": \"2\"}",
+                    label: "Primary"
+                },
+                color: "primary"
+            },
+            {
+                action: {
+                    type: "text",
+                    payload: "{\"button\": \"2\"}",
+                    label: "Secondary"
+                },
+                color: "secondary"
+            }
+        ]
+    ]
+})
 
 module.exports =(userid,text) => { 
      api('messages.send',{
          user_id: userid,
          message: text,
          access_token: TOKEN,
-         
+         keyboard
      })
      
 };
